2ca4a54c7d17229f5cf9bda217e7b9478902190b
Yaroslava Kalat <syaya@ukr.net>
2022-11-08 17:37:25 +0200

    optimization
    Додано dict для birthday_list і переписано друк цього списку як функцію print_birthday_list

--- homework_8.py
+++ homework_8.py
@@ -40,42 +40,41 @@ users = [
 ]
 
 def get_birthdays_per_week(users):
-    monday = []
-    tuesday = []
-    wednesday = []
-    thursday = []
-    friday = []
+    
+    birthday_list = {
+        'Monday': [],
+        'Tuesday': [],
+        'Wednesday': [],
+        'Thursday': [],
+        'Friday': [],
+    }
+   
     current_datetime = datetime.now()
     for user in users:
         birth_date = user.get('birthday')
-        if birth_date.month == current_datetime.month:
+        # пошук днів народження в поточному місяці
+        if birth_date.month == current_datetime.month: 
             birth_delta =  birth_date.day - current_datetime.day
+            # вибір днів народжень, які відбудуться протягом наступних 7 днів
             if birth_delta <= 7 and birth_delta > 0:
+                # перевірка на 29 лютого
                 if birth_date.day == 29 and birth_date.month == 2:
                     birth_date = datetime(current_datetime.year, 3, 1)
                     birth_delta =  birth_date.day - current_datetime.day
+                # визначення дня вітання
                 birth_date_new = current_datetime + timedelta(days=birth_delta)
                 if birth_date_new.weekday() == 0 or birth_date_new.weekday() == 5 or birth_date_new.weekday() == 6:
-                    monday.append(user.get('name'))
-                elif birth_date_new.weekday() == 1:
-                    tuesday.append(user.get('name'))
-                elif birth_date_new.weekday() == 2:
-                    wednesday.append(user.get('name'))
-                elif birth_date_new.weekday() == 3:
-                    thursday.append(user.get('name'))
-                elif birth_date_new.weekday() == 4:
-                    friday.append(user.get('name'))
-    if len(monday)>0: 
-        print('Monday:', ', '.join(map(str, monday)))
-    if len(tuesday)>0:
-        print('Tuesday:', ', '.join(map(str, tuesday)))
-    if len(wednesday)>0:
-        print('Wednesday:', ', '.join(map(str, wednesday)))
-    if len(thursday)>0:
-        print('Thursday:', ', '.join(map(str, thursday)))
-    if len(friday)>0:
-        print(f'Friday:', ', '.join(map(str, friday)))
-    
+                    birthday_list.get('Monday').append(user.get('name'))  
+                elif birth_date_new.weekday() in range(1,4):
+                    day_week = birth_date_new.strftime('%A')    
+                    birthday_list.get(day_week).append(user.get('name'))     
+   
+    print_birthday_list(birthday_list)          
+   # функція виводу списку іменинників
+def print_birthday_list(birthday_list: dict):
+    for key, val in birthday_list.items():
+        if val:
+            print(f"{key}: {', '.join(val)}")
     
 
 print(get_birthdays_per_week(users))
